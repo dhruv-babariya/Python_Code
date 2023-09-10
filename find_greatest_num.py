aeaef478b5d038d5841c47bcbeef278fedf753e0
dhruv-babariya <dhruvbabariya767@gmail.com>
2023-09-10 10:25:56 -1200

    Initial commit

---
+++ find_greatest_num.py
@@ -0,0 +1,19 @@
+A = int(input('Enter the first number: '))
+B = int(input('Enter the second number: '))
+C = int(input('Enter the third number: '))
+D = int(input('Enter the forth number: '))
+
+if(A>B):
+    f1 = A
+else:
+    f1 = B
+
+if(C>D):
+    f2 = C
+else:
+    f2 = D
+
+if(f1>f2):
+    print(f1, " is greatest")
+else:
+    print(f2, " is greatest")
