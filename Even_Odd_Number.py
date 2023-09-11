d44a62c12604d1845ef5c9ed47b4de6ac869a4b2
Dhruv babariya <90496279+dhruv-babariya@users.noreply.github.com>
2023-09-10 23:23:18 -1200

    Add files via upload

---
+++ Even_Odd_Number.py
@@ -0,0 +1,9 @@
+# Identify whether inputed number is odd or even....4
+
+a = int(input('Enter the number: '))
+
+if(a%2) == 0:
+    print("The number is Even...")
+
+else:
+    print("The number is Odd...")
