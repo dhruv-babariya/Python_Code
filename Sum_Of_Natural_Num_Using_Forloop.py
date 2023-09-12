e63014fd148fcae44ab93823ab2c98f914658f72
Dhruv babariya <90496279+dhruv-babariya@users.noreply.github.com>
2023-09-12 05:43:26 -1200

    Sum_Of_Natural_Num_Using_Forloop.py

--- Sum_Of_Natural_Num_Using_Forloop.py
+++ Sum_Of_Natural_Num_Using_Forloop.py
@@ -1,3 +1,6 @@
+# Write a program about calculation of use input number until 0 
+# (means, if user input 4 then it will sum 4 + 3 + 2 + 1 = 10)
+
 num = int(input("Enter a number: "))
 sum = 0
 for i in range(0, num+1):
