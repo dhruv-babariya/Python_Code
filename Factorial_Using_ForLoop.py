5e31c2807372b0594fa8fd09531ae792e7c54fa7
Dhruv babariya <90496279+dhruv-babariya@users.noreply.github.com>
2023-09-12 05:45:36 -1200

    Factorial_Using_ForLoop.py

--- Factorial_Using_ForLoop.py
+++ Factorial_Using_ForLoop.py
@@ -1,3 +1,6 @@
+# Write a program which find a factorial number provided by user
+# (Means, if user enter 4 then it will work like 4 * 3 * 2 * 1 = 24)
+
 num = int(input("Enter a number: "))
 
 factorial = 1
