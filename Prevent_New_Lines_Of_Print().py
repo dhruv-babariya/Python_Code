3759481d290d98d3166001648d0772895812979d
Dhruv babariya <90496279+dhruv-babariya@users.noreply.github.com>
2023-09-13 05:45:07 -1200

    Prevent_New_Lines_Of_Print().py

---
+++ Prevent_New_Lines_Of_Print().py
@@ -0,0 +1,13 @@
+# By doing this method, we can print multiple lines in a single line
+print("Hello", end=" ")
+print("my", end=" ")
+print("dear", end=" ")
+print("friends", end=" ")
+
+
+# If you we don't write { end=" " } this then it will print like these...
+'''
+Hello 
+my 
+dear 
+friends'''
